da0cc891703aedb364ee34b094a35f4846806723
Elias Pekkala <eliaspekkala98@gmail.com>
2019-04-02 18:39:46 +0200

    Set up express server

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 'use strict'
 
-console.log('Hello World!')
+const express = require('express')
+const app = express()
+const port = 3000
+
+// Serve static files
+app.use(express.static('public'))
+
+// Routes
+app.get('/', (req, res) => res.send('index.html'))
+
+// Start server
+app.listen(port, () => console.log('Server running at http://localhost:3000/'))
