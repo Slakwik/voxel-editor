02d27fc7ea55b6a408e8162f7329971b4c0cb5c6
Elias Pekkala <eliaspekkala98@gmail.com>
2019-04-12 11:54:16 +0200

    Add color generator and simplify button creation

--- src/my-palette.js
+++ src/my-palette.js
@@ -16,25 +16,32 @@ class Palette extends LitElement {
         padding: 10px;
         border-radius: 2px;
         margin: 4px 4px;
+        font-size: 8px;
       }
     `
+  }
+
+  constructor () {
+    super()
+
+    this.colors = []
+
+    // Create rainbow with HSL colors
+    for (let i = 0; i < 360; i += 30) {
+      const hue = i
+      const saturation = 60
+      const lightness = 60
+
+      this.colors.push(`hsl(${hue}, ${saturation}, ${lightness})`)
+    }
+
+    console.log(this.colors)
   }
 
   render () {
     return html`
       <div>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
+        ${this.colors.map(i => html`<button>${i}</button>`)}
       </div>
     `
   }
