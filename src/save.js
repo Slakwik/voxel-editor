5c990cd3f8c705b8d2a8bf9d46fb4fe84765049b
Elias Pekkala <eliaspekkala98@gmail.com>
2019-04-18 11:38:45 +0200

    Implement saving scene data to localstorage

--- src/save.js
+++ src/save.js
@@ -2,6 +2,13 @@ const THREE = window.THREE
 
 function save (scene) {
   const exporter = new THREE.GLTFExporter()
+  const options = { }
+
+  exporter.parse(scene, (gltf) => {
+    const gltfJSON = JSON.stringify(gltf)
+    window.localStorage.setItem('scene', gltfJSON)
+    console.log('Scene saved...')
+  }, options)
 }
 
 export default save
