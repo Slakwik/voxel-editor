23f35becdabf2dae5bdc450eb5ab1bf9f3ec613c
Elias Pekkala <eliaspekkala98@gmail.com>
2019-04-24 10:46:15 +0200

    Delete old testing setup

--- test/createHSLColor.js
+++
@@ -1,3 +0,0 @@
-export default function createHSLColor (hue, saturation, lightness) {
-  return `hsl(${hue}, ${saturation}%, ${lightness}%)`
-}
--- test/createHSLColor.test.js
+++
@@ -1,9 +0,0 @@
-import createHSLColor from './createHSLColor.js'
-
-describe('createColorArray()', () => {
-  it('returns "hsl(10, 10%, 10%)" for the params (10, 10, 10)', () => {
-    const expected = 'hsl(10, 10%, 10%)'
-    const actual = createHSLColor(10, 10, 10)
-    chai.assert.equal(expected, actual) // eslint-disable-line
-  })
-})
