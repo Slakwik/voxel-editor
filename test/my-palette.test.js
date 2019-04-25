7ca930f45ea8560bd87ea12ac7f4b0c94697da54
Elias Pekkala <eliaspekkala98@gmail.com>
2019-04-25 10:33:30 +0200

    Add tests for my-palette

--- test/my-palette.test.js
+++ test/my-palette.test.js
@@ -7,11 +7,36 @@ describe('my-palette', () => {
     await page.goto('http://localhost:8081')
   })
 
-  describe('createColorArray()', () => {
+  it('is added to the dom', async () => {
+    const element = await page.evaluate(`${elementPath}`)
+    global.assert.isOk(element)
+  })
+
+  describe('createHSLColor()', () => {
+    it('returns a string', async () => {
+      const expectedType = 'string'
+      const actualType = await page.evaluate(`${elementPath}.createHSLColor(10, 10, 10)`)
+      global.assert.typeOf(actualType, expectedType)
+    })
+
     it('returns "hsl(10, 10%, 10%)" for the params (10, 10, 10)', async () => {
       const expected = 'hsl(10, 10%, 10%)'
       const actual = await page.evaluate(`${elementPath}.createHSLColor(10, 10, 10)`)
       global.assert.strictEqual(actual, expected)
+    })
+  })
+
+  describe('createColorArray()', () => {
+    it('returns an array', async () => {
+      const expectedType = 'array'
+      const actualType = await page.evaluate(`${elementPath}.createColorArray(10, 10, 10)`)
+      global.assert.typeOf(actualType, expectedType)
+    })
+
+    it('returns 36 colors when hueStepLength is set to 10', async () => {
+      const expectedLength = 36
+      const result = await page.evaluate(`${elementPath}.createColorArray(10, 0, 0)`)
+      global.assert.lengthOf(result, expectedLength)
     })
   })
 
