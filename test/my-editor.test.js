78b88cfc90630460ef48e460efb7019463f4560e
Elias Pekkala <eliaspekkala98@gmail.com>
2019-04-25 12:03:23 +0200

    Add tests for isCube

--- test/my-editor.test.js
+++ test/my-editor.test.js
@@ -12,6 +12,26 @@ describe('my-editor', () => {
     global.assert.isOk(element)
   })
 
+  describe('isCube()', () => {
+    it('returns a boolean', async () => {
+      const expectedType = 'boolean'
+      const actualType = await page.evaluate(`${elementPath}.isCube({ name: 'Sky: 12' })`)
+      global.assert.typeOf(actualType, expectedType)
+    })
+
+    it('returns true if object name starts with Cube', async () => {
+      const expected = true
+      const actual = await page.evaluate(`${elementPath}.isCube({ name: 'Cube: 10' })`)
+      global.assert.strictEqual(actual, expected)
+    })
+
+    it('returns false if object name doesn\'t start with Cube', async () => {
+      const expected = false
+      const actual = await page.evaluate(`${elementPath}.isCube({ name: 'Grid: 4' })`)
+      global.assert.strictEqual(actual, expected)
+    })
+  })
+
   after(async () => {
     await page.close()
   })
