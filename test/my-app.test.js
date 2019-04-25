11b954fc9e59ef4b0a7731e462a2a2fb757e6577
Elias Pekkala <eliaspekkala98@gmail.com>
2019-04-25 14:49:20 +0200

    Add tests for my-app

--- test/my-app.test.js
+++ test/my-app.test.js
@@ -12,6 +12,30 @@ describe('my-app', () => {
     global.assert.isOk(element)
   })
 
+  it('covers the whole page width', async () => {
+    const expected = await page.viewport().width
+    const actual = await page.evaluate(`${elementPath}.getBoundingClientRect().width`)
+    global.assert.strictEqual(actual, expected)
+  })
+
+  it('covers the whole page height', async () => {
+    const expected = await page.viewport().height
+    const actual = await page.evaluate(`${elementPath}.getBoundingClientRect().height`)
+    global.assert.strictEqual(actual, expected)
+  })
+
+  it('starts with build mode selected', async () => {
+    const expected = 'build-mode'
+    const actual = await page.evaluate(`${elementPath}.mode`)
+    global.assert.strictEqual(actual, expected)
+  })
+
+  it('starts with the color yellow selected', async () => {
+    const expected = 'hsl(60, 90%, 60%)'
+    const actual = await page.evaluate(`${elementPath}.color`)
+    global.assert.strictEqual(actual, expected)
+  })
+
   after(async () => {
     await page.close()
   })
