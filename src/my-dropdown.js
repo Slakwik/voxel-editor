96b1c50ffe471de905a47bfb3eb03820516d9d26
Elias Pekkala <eliaspekkala98@gmail.com>
2019-05-01 10:05:36 +0200

    Fix dropdown styles

--- src/my-dropdown.js
+++ src/my-dropdown.js
@@ -6,17 +6,20 @@ class MyDropdown extends LitElement {
       :host {
         display: inline;
       }
-      div {
+      button {
         background-color: #039372;
         color: white;
         padding: 4px 10px;
-        margin-right: 5px;
+        margin: 0 5px 0 3px;
         border: none;
         border-radius: 3px;
-        display: inline
+        display: inline;
       }
       .content {
         display: none;
+      }
+      .content button {
+        background-color: #04c89a;
       }
       .hidden {
         display: inline;
