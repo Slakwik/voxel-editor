749f9a82ec4c5c00032e733eb4fcc3bb33efe4fb
Elias Pekkala <eliaspekkala98@gmail.com>
2019-05-18 12:17:05 +0200

    Add tests for my-dropdown button reveal

--- test/my-dropdown.test.js
+++ test/my-dropdown.test.js
@@ -12,6 +12,17 @@ describe('my-dropdown', () => {
     global.assert.isOk(element)
   })
 
+  it('should have its buttons hidden', async () => {
+    const classList = await page.evaluate(`document.querySelector("body > my-app").shadowRoot.querySelector("my-appbar").shadowRoot.querySelector("my-dropdown:nth-child(2)").shadowRoot.querySelector("div").classList`)
+    global.assert.isTrue(Object.values(classList).includes('hidden'))
+  })
+
+  it('should reveal its buttons when clicked', async () => {
+    await page.evaluate(`${elementPath}.shadowRoot.querySelector("button").click()`)
+    const classList = await page.evaluate(`document.querySelector("body > my-app").shadowRoot.querySelector("my-appbar").shadowRoot.querySelector("my-dropdown:nth-child(2)").shadowRoot.querySelector("div").classList`)
+    global.assert.isNotTrue(Object.values(classList).includes('hidden'))
+  })
+
   after(async () => {
     await page.close()
   })
