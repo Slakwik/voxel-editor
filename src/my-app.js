014ae251f64ebc48b7d02a3aefc32ee9bcc7e95c
Elias Pekkala <eliaspekkala98@gmail.com>
2019-05-20 13:26:54 +0200

    Add documentation comments

--- src/my-app.js
+++ src/my-app.js
@@ -1,3 +1,12 @@
+/**
+ * Module for the main app component.
+ *
+ * @module src/my-app
+ * @author Elias Pekkala
+ * @version 1.0.0
+ */
+
+// Imports modules and other components.
 import { LitElement, html } from 'lit-element'
 import { loadSettings, saveSettings } from './settings.js'
 import './three'
@@ -7,33 +16,67 @@ import './my-sidebar'
 import './my-palette'
 import './my-settings'
 
+/**
+ * The main app component.
+ *
+ * @class MyApp
+ * @extends {LitElement}
+ */
 class MyApp extends LitElement {
+  /**
+   * The component properties.
+   *
+   * @readonly
+   * @static
+   * @memberof MyApp
+   */
   static get properties () {
     return {
+      // The currently active mode.
       mode: { type: String },
+      // The currently active color for building / coloring.
       color: { type: String },
+      // The latest menu action.
       action: { type: String }
     }
   }
 
+  /**
+   * Creates an instance of MyApp.
+   *
+   * @memberof MyApp
+   */
   constructor () {
     super()
+
+    // Sets the default mode and color.
     this.mode = 'build-mode'
     this.color = 'hsl(60, 90%, 60%)'
 
+    // Sets and saves default settings if there aren't any user settings.
     if (!loadSettings()) {
       saveSettings({ antiAliasing: true, pbrMaterials: true, skyBackground: true })
     }
   }
 
+  /**
+   * Renders a template inside the components shadow root.
+   *
+   * @returns {TemplateResult} The template to render.
+   * @memberof MyApp
+   */
   render () {
     return html`
       <my-appbar @menu-action="${(e) => { this.action = e.detail.message }}"></my-appbar>
+      
       <my-editor .mode=${this.mode} .color=${this.color} action=${this.action}></my-editor>
+      
       <my-sidebar @mode-change="${(e) => { this.mode = e.detail.message }}" @color-change="${(e) => { this.color = e.detail.message }}"></my-sidebar>
+      
       ${this.action === 'settings' ? html`<my-settings></my-settings>` : ''}
     `
   }
 }
 
+// Registers the custom element with the browser.
 window.customElements.define('my-app', MyApp)
